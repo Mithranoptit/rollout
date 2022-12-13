df71b0bd7b05ab7ea5debe941b3390be7a532733
Mithranoptit <112661692+Mithranoptit@users.noreply.github.com>
2022-12-13 15:57:10 +0530

    corrected blob to raw

--- kbsetup.sh
+++ kbsetup.sh
@@ -2,10 +2,10 @@
 mkdir t3mp
 echo "Downloading the source"
 cd t3mp
-wget https://github.com/Mithranoptit/rollout/blob/main/installer.xml
-wget https://github.com/Mithranoptit/rollout/blob/main/bdconfigure
-wget https://github.com/Mithranoptit/rollout/blob/main/installer
-wget https://github.com/Mithranoptit/rollout/blob/main/KcsSetup.sh
+wget https://github.com/Mithranoptit/rollout/raw/main/installer.xml
+wget https://github.com/Mithranoptit/rollout/raw/main/bdconfigure
+wget https://github.com/Mithranoptit/rollout/raw/main/installer
+wget https://github.com/Mithranoptit/rollout/raw/main/KcsSetup.sh
 echo "installing Bitdefender"
 chmod 777 installer
 ./installer
